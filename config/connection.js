93fc4c962d97671bbf70557d840eb3ae096151cd
Marc Strong <marcstrong@yahoo.com>
2020-05-04 22:55:54 -0500

    hook project with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // add dependencies npm package mysql
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (process.execArgv.JAWSDB_URL) {
+  connection = mysqul.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
+
 
 connection.connect(function(err) {
   if (err) {
